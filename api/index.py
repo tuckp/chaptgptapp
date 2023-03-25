ccd1a4bde372fbdd98d35ed5367945ece04e9f9e
tuckp <37231371+tuckp@users.noreply.github.com>
2023-03-25 16:14:04 +0700

    final chatgpt test project on vercel

--- api/index.py
+++ api/index.py
@@ -1,71 +1,64 @@
 import openai
-import config
-
-
-from flask import Flask
+import os
+from flask import Flask, jsonify, render_template, request
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
+initialContext = ""
+
+# split the text into a list of sentences with whitespace
+sentences = initialContext.split('\n')
+
+# remove whitespace from the beginning and end of each sentence
+sentences = [s.strip() for s in sentences]
+
+def get_model_reply(query, context=[]):
+    # your secret token goes here
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+    # combines the new question with a previous context
+    context += [query]
+    print(context)
+
+    # given the most recent context (4096 characters)
+    # continue the text up to 2048 tokens ~ 8192 charaters
+    completion = openai.Completion.create(
+        engine='text-davinci-003',  # one of the most capable models available
+        prompt='\n\n'.join(context)[:4096],
+        max_tokens=2048,
+        temperature=0.0,  # Lower values make the response more deterministic
+    )
+
+    # append response to context
+    response = completion.choices[0].text.strip('\n')
+    context += [response]
+
+    # list of (user, bot) responses. We will use this format later
+    responses = [(u, b) for u, b in zip(context[::2], context[1::2])]
+
+    return responses, context
+
+
 @app.route('/')
 def home():
+    return render_template('index.html')
 
-    openai.api_key = config.API_KEY # your token goes here
 
-    def get_model_reply(query, context=[]):
-        # combines the new question with a previous context
-        context += [query]
-        
-        # given the most recent context (4096 characters)
-        # continue the text up to 2048 tokens ~ 8192 charaters
-        completion = openai.Completion.create(
-            engine='text-davinci-003', # one of the most capable models available
-            prompt='\n\n'.join(context)[:4096],
-            max_tokens = 2048,
-            temperature = 0.0, # Lower values make the response more deterministic
-        )
-        
-        # append response to context
-        response = completion.choices[0].text.strip('\n')
-        context += [response]
-        
-        # list of (user, bot) responses. We will use this format later
-        responses = [(u,b) for u,b in zip(context[::2], context[1::2])]
-        
-        return responses, context
+@app.route('/chat', methods=['POST'])
+def chat():
+    if (request.json.get('query')):
+        # get the query from the request
+        query = request.json.get('query')
 
-    ####################################################################################################################################################################################
+        # get the context from the request
+        context = request.json.get('context') or []
 
-    query = 'Which is the largest country by area in the world?'
-    responses, context = get_model_reply(query, context=[])
+        # get the response from the model
+        responses, context = get_model_reply(query, context)
 
-    # print('<USER> ' + responses[-1][0])
-    # print('<BOT> ' + responses[-1][1])
-
-    # OUTPUT:
-    #
-    # <USER> Which is the largest country by area in the world?
-    # <BOT> The largest country by area in the world is Russia, with a total area of 17,098,242 square kilometers (6,601,668 square miles).
-
-    ####################################################################################################################################################################################
-
-    import gradio as gr
-
-    # defines a basic dialog interface using Gradio
-    with gr.Blocks() as dialog_app:
-        chatbot = gr.Chatbot() # dedicated "chatbot" component
-        state = gr.State([]) # session state that persists across multiple submits
-        
-        with gr.Row():
-            txt = gr.Textbox(
-                show_label=False, 
-                placeholder="Enter text and press enter"
-            ).style(container=False)
-
-        txt.submit(get_model_reply, [txt, state], [chatbot, state])
-
-    # launches the app in a new local port
-    dialog_app.launch()
-
-    ####################################################################################################################################################################################
-
-    return
+        # return the response
+        return jsonify({'response': responses[-1][1], 'context': context})
+    else:
+        return jsonify({'response': 'Error: No query provided'})
